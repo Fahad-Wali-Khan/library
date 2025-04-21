fb89da93714c66fd326854d05e0de62daaade23e
fahad-wali-khan <94318061+Fahad-Wali-Khan@users.noreply.github.com>
2025-04-22 01:24:44 +0530

    final commit

---
+++ playground/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+// import { Button, Card } from '../src' // Import from your library source
+// import './App.css'
+// import '../../src/Components/Login.jsx'
+// import Login from '../../src/Components/Login.jsx'
+import LoginPage from '../../src/Modules/Login/LoginPage'
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-red-300'>Hello V</h1>
+      <LoginPage/>
+    </div>
+  )
+}
+
+export default App
